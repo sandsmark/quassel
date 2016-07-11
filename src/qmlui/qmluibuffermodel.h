30c11227c25594d056721610a3b09b96e6c55d0d
Christian <christian@twotoasts.de>
2016-07-11 20:14:51 +0200

    Expose core connection status to buffer model

--- src/qmlui/qmluibuffermodel.h
+++ src/qmlui/qmluibuffermodel.h
@@ -7,17 +7,23 @@ class QmlUiBufferModel : public QSortFilterProxyModel
 {
     Q_OBJECT
 
+    Q_PROPERTY(bool connected READ connected NOTIFY connectedChanged)
+
 public:
     QmlUiBufferModel(QObject *parent = 0);
 
     // QAbstractListModel
     QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
     QHash<int, QByteArray>roleNames() const;
+
+Q_SIGNALS:
+    void connectedChanged();
 protected:
 
 protected slots:
 
 private:
+    bool connected() const;
 };
 
 #endif
