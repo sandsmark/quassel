858d4b109de089f2e6171f7b15148e17b2dc5dcf
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-07-20 01:45:17 +0200

    rough fix for sorting and filtering, copied from bufferviewfilter

--- src/qmlui/qmluibuffermodel.h
+++ src/qmlui/qmluibuffermodel.h
@@ -3,6 +3,8 @@
 
 #include "buffermodel.h"
 
+class ClientBufferViewConfig;
+
 class QmlUiBufferModel : public QSortFilterProxyModel
 {
     Q_OBJECT
@@ -13,18 +15,25 @@ public:
     QmlUiBufferModel(QObject *parent = 0);
 
     // QAbstractListModel
-    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
-    QHash<int, QByteArray>roleNames() const;
+    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray>roleNames() const override;
+
+    void setConfig(ClientBufferViewConfig *config);
 
 Q_SIGNALS:
     void connectedChanged();
 protected:
-    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
+    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const override;
+    bool lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const override;
 
 protected slots:
 
 private:
     bool connected() const;
+    bool bufferIdLessThan(const BufferId &left, const BufferId &right) const;
+    bool bufferLessThan(const QModelIndex &source_left, const QModelIndex &source_right) const;
+
+    ClientBufferViewConfig *m_config;
 };
 
 #endif
